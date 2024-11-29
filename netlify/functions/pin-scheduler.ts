fcfa61d20a98dd500b46d747be846c31ca70ce3b
davittrend <davidtrendovski@gmail.com>
2024-11-29 22:27:48 +0100

    Update pin-scheduler.ts

--- netlify/functions/pin-scheduler.ts
+++ netlify/functions/pin-scheduler.ts
@@ -11,7 +11,7 @@ if (!getApps().length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
   });
 }
 
