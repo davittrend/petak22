7ec78766f35a9cdcdfe6ef64f2926f5fd5be6e6e
davittrend <davidtrendovski@gmail.com>
2024-11-29 23:19:07 +0100

    Update init.ts

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { firebaseConfig } from './config';
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase only if no apps exist
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
