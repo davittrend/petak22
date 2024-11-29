d07fcc6f2b3cd4521aa0bfe1d48a6822bb4006f1
davittrend <davidtrendovski@gmail.com>
2024-11-30 00:04:37 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Initialize Firebase before rendering
+import './lib/firebase/init';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
