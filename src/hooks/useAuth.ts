9184e260d6f1a20e7f4fe7e9e5662a1f75bf215f
davittrend <davidtrendovski@gmail.com>
2024-12-01 12:22:26 +0100

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
 import { useAccountStore } from '@/lib/store';
